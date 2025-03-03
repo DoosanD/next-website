c382a9c74aa5e5ce978bc98188183204044b6c0c
DoosanD <davicdusan@gmail.com>
2025-03-03 22:07:47 +0100

    03/03/2025

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
 import Image from 'next/image'
 import Link from 'next/link'
+import ProductCard from './components/ProductCard/ProductCard'
 
 export default function Home() {
   return (
     <main>
-      <h1>Hello there dipshit!</h1>
+      <h1>This is Homepage</h1>
       <a href="/users">USERs</a>
       <Link href="/users">Users_COMP</Link>
+      <Link href="/">Home</Link>
+
+      <ProductCard />
     </main>
   )
 }
--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import Link from 'next/link';
 
-const UsersPage = () => {
+interface User {
+  id: number;
+  name: string;
+  email: string;
+}
+
+const UsersPage = async () => {
+  // fetch users data from API
+  const res = await  fetch(
+    'https://jsonplaceholder.typicode.com/users', 
+     { next: {revalidate: 10}}
+  );
+  const users: User[] = await res.json();
+
   return (
-    <div>UsersPage</div>
+    <>
+    <Link href="/">Home</Link>
+      <h1>Users Page</h1>
+      <p>{new Date().toLocaleTimeString()}</p>
+      {users.map(users => (
+        <div key={users.id}>
+          <h2>{users.name}</h2>
+          <p>{users.email}</p>
+        </div>
+      ))}
+    </>
   )
 }
 

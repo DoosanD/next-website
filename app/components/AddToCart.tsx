cf650081b7b38037441408bd72301304490425ad
DoosanD <davicdusan@gmail.com>
2025-03-10 19:30:32 +0100

    10/03/2025

--- app/components/AddToCart.tsx
+++ app/components/AddToCart.tsx
@@ -5,6 +5,7 @@ const AddToCart = () => {
   return (
     <div>
       <button className='btn btn-warning test' onClick={()=>{console.log('Click!')}}>Add to Cart</button>
+      <div className='btn border-t-cyan-300' onClick={()=>{alert("does it work")}}>Test</div>
     </div>
   )
 }
